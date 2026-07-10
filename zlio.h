5b055e17318a0873d4c2771bbc68e75988f3bac1
Zile Lai <85780409+zilelai@users.noreply.github.com>
2026-07-10 19:41:17 +0800

    Create zlio.h

---
+++ zlio.h
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+#ifndef ZLIO_H
+#define ZLIO_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+int out(const char *format, ...);
+
+#ifdef __cplusplus
+
+}
+
+
+#endif
+#endif
+
+
+
+
