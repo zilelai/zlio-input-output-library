09f9c9647db9f3ef96fbdabb03da360869c787d1
Zile Lai <85780409+zilelai@users.noreply.github.com>
2026-07-30 18:56:18 +0800

    Update zlio.h

--- zlio.h
+++ zlio.h
@@ -1,24 +1,37 @@
-#include <stdarg.h>
-#include <unistd.h>
-
 #ifndef ZLIO_H
 #define ZLIO_H
 
-#ifdef __cplusplus
-extern "C"{
-#endif
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdbool.h> 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int out(const char *format, ...);
 
+typedef enum {
+    INPUTINT,
+    INPUTDOUBLE,
+    INPUTBOOL,
+    INPUTSTR
+} InputType;
+
+typedef struct {
+    InputType type;
+    union {
+        int intvalue;
+        double doublevalue;
+        bool boolvalue;
+        char stringvalue[256];
+    } data;
+} InputValue;
+
+InputValue in(const char *prompt);
+
 #ifdef __cplusplus
-
 }
-
-
-#endif
 #endif
 
-
-
-
+#endif
